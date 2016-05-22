480b40ec4ffd4758cac5820523f4b1985052dab7
tsaikd <tsaikd@gmail.com>
2016-05-22 13:59:44 +0800

    add docker related files
    Signed-off-by: tsaikd <tsaikd@gmail.com>

---
+++ docker/build-in-docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+PN="${BASH_SOURCE[0]##*/}"
+PD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+projorg="tsaikd"
+projname="go-raml-mocker"
+
+renice 15 $$
+cd "${PD}/.."
+
+if ! type gobuilder &>/dev/null ; then
+	go get -v "github.com/tsaikd/gobuilder"
+fi
+
+gobuilder
+
+if [ "${GITHUB_TOKEN}" ] ; then
+	echo "[$(date -Iseconds)] ${projname} release on github"
+	if ! type github-release &>/dev/null ; then
+		go get -v "github.com/aktau/github-release"
+	fi
+	version="$(./${projname} -v | grep -Eo "version [0-9\.]+" | cut -c9-)"
+	github-release release \
+		--user "${projorg}" \
+		--repo "${projname}" \
+		--tag "${version}"
+	github-release upload \
+		--user "${projorg}" \
+		--repo "${projname}" \
+		--tag "${version}" \
+		--name "${projname}-$(uname -s)-$(uname -m)" \
+		--file "${projname}"
+fi
